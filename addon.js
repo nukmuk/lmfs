5581cbf6bf674b2c09c5cd8658effb667f75c849
NUKMUK <rasmus.hintzmadsen@gmail.com>
2020-12-31 08:24:35 +0200

    getslugs working hopefully

--- addon.js
+++ addon.js
@@ -19,26 +19,36 @@ const builder = new addonBuilder({
 
 // takes function(args)
 builder.defineStreamHandler(async function (args) {
+
+    let streams = [];
+
     try {
 
+        // get imdb number from args.id using regex
+        // args.id format: tt0123456:season:episode
         const show_imdb = args.id.match(/tt\d+/);
-        const show_type = args.type;
-        console.log(show_type);
+
+        console.log("args.type: " + args.type);
 
         // get name of the show from cinemeta
-        const cinemeta_url = `https://v3-cinemeta.strem.io/meta/${show_type}/${show_imdb}.json`;
-        console.log("cinemeta url: " + cinemeta_url);
-        const cinemeta_html = await got(cinemeta_url);
-        const cinemeta_json = JSON.parse(cinemeta_html.body);
-        const show_name = cinemeta_json["meta"]["name"];
-        console.log("show name from cinemeta: " + show_name);
+        const show_name = await getShowName(show_imdb, args.type);
+        console.log("SHOW NAME: " + show_name);
 
-        if (args.type == "movie") {
+        // get streams
+        if (args.type == "movie") {          // for movies
             console.log("show is movie");
-            // getStreams(show_imdb, show_name, true);
-        } else if (args.type == "series") {
+            const returnedStreams = scraper.getStreams(show_imdb, show_name, true);
+            streams.push(returnedStreams);
+
+        } else if (args.type == "series") {  // for series
             console.log("show is series");
-            // getStreams(show_imdb, show_name, false, show_episode, show_season);
+
+            // get season and episode number from args.id
+            const show_season = args.id.match(/tt.+:(.+):.+/)[1];
+            const show_episode = args.id.match(/tt.+:.+:(.+)/)[1];
+
+            const returnedStreams = scraper.getStreams(show_imdb, show_name, false, show_episode, show_season);
+            streams.push(returnedStreams);
         }
     } catch (err) {
         console.error(err);
@@ -49,3 +59,15 @@ builder.defineStreamHandler(async function (args) {
 
 serveHTTP(builder.getInterface(), { port: process.env.PORT || 7000 });
 //publishToCentral("https://your-domain/manifest.json") // <- invoke this if you want to publish your addon and it's accessible publically on "your-domain"
+
+
+// input tt0123456
+async function getShowName(show_imdb, show_type) {
+    const cinemeta_url = `https://v3-cinemeta.strem.io/meta/${show_type}/${show_imdb}.json`;
+    console.log("cinemeta url: " + cinemeta_url);
+    const cinemeta_html = await got(cinemeta_url);
+    const cinemeta_json = JSON.parse(cinemeta_html.body);
+    const show_name = cinemeta_json["meta"]["name"];
+    console.log("got show name from cinemeta: " + show_name);
+    return show_name;
+}
--- scraper.js
+++ scraper.js
@@ -1,7 +1,97 @@
 const got = require("got");
 
-module.exports = getStreams;
+module.exports = { getStreams };
 
-async function getStreams() {
+const addon_name = "LookMovie.io";
 
+const URL = "https://lookmovie.io";
+const URL_FP = "https://false-promise.lookmovie.io";
+
+// not done
+async function getStreams(show_imdb, show_name, show_isMovie, show_episode, show_season) {
+    try {
+
+        let streams = [];
+
+        const slugs = await getSlugs(show_name, show_isMovie);
+
+        if (show_isMovie == true) {
+            // todo
+        } else if (show_isMovie == false) {
+            // todo
+        } else {
+            return console.error("show_isMovie not true or false");
+        }
+        console.log("slugs amount: " + slugs.length);
+
+
+        return streams;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// done (maybe. made at klo 7 in the morning)
+async function getSlugs(show_name, show_isMovie) {
+    try {
+        console.log("show name: " + show_name);
+        const show_encodedname = encodeURIComponent(show_name);
+
+        let searchURL;
+
+        if (show_isMovie == true) {
+            searchURL = `${URL}/api/v1/movies/search/?q=${show_encodedname}`;
+        } else if (show_isMovie == false) {
+            searchURL = `${URL}/api/v1/shows/search/?q=${show_encodedname}`;
+        } else {
+            console.error("show_isMovie not true or false");
+            return [];
+        }
+
+        console.log(searchURL);
+        const search_results = await got(searchURL);
+
+        let search_parsedresults = JSON.parse(search_results.body);
+        console.log("JSON.parsed search results: " + JSON.stringify(search_parsedresults, null, 4));
+
+        // get part with data about results from json
+        let results = search_parsedresults["result"];
+
+        // go through all results and push slug to slugs[] from each
+        let slugs = [];
+        results.forEach(result => {
+            const slug = result["slug"];
+            console.log("pushing slug to slugs array: " + slug);
+            slugs.push(slug);
+        });
+
+        return slugs;
+
+
+    } catch (err) {
+        console.error(err);
+        return [];
+    }
+}
+
+
+// not done
+async function getMovieID(slugs) {
+
+}
+
+// not done
+async function getSeriesID(slugs) {
+    for (const slug of slugs) {
+        try {
+            console.log("trying slug: " + slug);
+            const show_id = await getShowID(show_slug)
+            console.log("show_id: " + show_id);
+
+
+        } catch (err) {
+            console.error(err);
+            console.log("Didn't find show: " + slug);
+        }
+    }
 }
