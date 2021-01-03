e9d6cf9caf5a9fdefd4c86e8ed05ab3b49166cea
NUKMUK <rasmus.hintzmadsen@gmail.com>
2021-01-03 08:10:37 +0200

    async working but copy pasted needs fixing idk

--- scraper.js
+++ scraper.js
@@ -21,7 +21,7 @@ async function getStreams(show_imdb, show_name, show_isMovie, show_episode, show
             const movie_ids = await getMovieID(slugs);
             console.log(movie_ids);
         } else if (show_isMovie == false) {  // series
-            const show_ids = await getSeriesID(slugs, show_episode, show_season);
+            const show_ids = await getSeriesIDAsync(slugs, show_episode, show_season);
             console.log(show_ids);
         } else {
             return console.error("getStreams(): show_isMovie not true or false");
@@ -35,7 +35,7 @@ async function getStreams(show_imdb, show_name, show_isMovie, show_episode, show
     }
 }
 
-// done (maybe, made at klo 7 in the morning)
+// done
 async function getSlugs(show_name, show_isMovie) {
     try {
         console.log("show name: " + show_name);
@@ -116,44 +116,79 @@ async function getMovieID(slugs) {
 }
 
 // done
-async function getSeriesID(slugs, show_episode, show_season) {
+async function getSeriesIDAsync(slugs, show_episode, show_season) {
 
     let shows = [];
 
-    for (const slug of slugs) {
+    const slugsURLs = slugs.map(x => `${URL}/shows/view/${x}`);
+
+    const slugs_html = await getParallel(slugsURLs);
+
+    console.log("length of slugs_html: " + slugs_html.length);
+
+    for (const html of slugs_html) {
         try {
 
             let show = {};
-            show["slug"] = slug;
 
-            console.log("trying series slug: " + slug);
-
-            let URL_SLUG_SHOWS = `${URL}/shows/view/${slug}`;
-
-            const html = await got(URL_SLUG_SHOWS);
-            const html_body = html.body;
-
-
+            console.log(html);
             const EPISODE_DATA_REGEX = new RegExp(
                 `title: '([^']+)', episode: '${show_episode}', id_episode: (\\d+), season: '${show_season}'`
             );
 
 
             // same for movies and series
-            show["title"] = html.body.match(/title: '([^']+)'/)[1];
-            show["year"] = html.body.match(/year: '(\d+)'/)[1];
+            show["slug"] = html.match(/slug: '([^']+)'/)[1];
+            show["title"] = html.match(/title: '([^']+)'/)[1];
+            show["year"] = html.match(/year: '(\d+)'/)[1];
 
             // only for series
-            show["episode_title"] = html.body.match(EPISODE_DATA_REGEX)[1];
-            show["episode_id"] = html_body.match(EPISODE_DATA_REGEX)[2];
-            show["show_id"] = html_body.match(/id_show: (\d+)/)[1];
+            show["episode_title"] = html.match(EPISODE_DATA_REGEX)[1];
+            show["episode_id"] = html.match(EPISODE_DATA_REGEX)[2];
+            show["show_id"] = html.match(/id_show: (\d+)/)[1];
 
             shows.push(show);
 
         } catch (err) {
             console.error(err);
-            console.warn("Didn't find episode " + show_episode + " of season " + show_season + " of show " + slug);
+            console.warn("Didn't find episode " + show_episode + " of season " + show_season + " of some show");
         }
     }
     return shows;
+}
+
+// sends http requests in parallel. returns array of got html response object things
+async function convertSlugsToHTML(slugs) {
+
+    const slugsURLs = slugs.map(x => `${URL}/shows/view/${x}`);
+
+    console.warn(slugsURLs);
+
+    const promises = slugsURLs.map(got);
+    let result = [];
+    result = await Promise.allSettled(promises);
+    console.log("ASDAJDKSSAKLJDAHSKJDSJKDHASKJDHASD");
+    console.warn(result);
+    return result;
+
+}
+
+async function requestAsync(url) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await got(url);
+            resolve(response.body);
+        } catch (error) {
+            return reject(error);
+        }
+    });
+}
+
+async function getParallel(slugs) {
+    try {
+        var data = await Promise.all(slugs.map(requestAsync));
+    } catch (err) {
+        console.error("ERROR: " + err);
+    }
+    return data;
 }
